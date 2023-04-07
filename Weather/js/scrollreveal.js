2aede2a1f97842d54796b0a02295bead3fab4da1
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-04-07 20:12:31 -0300

    Add files via upload

---
+++ Weather/js/scrollreveal.js
@@ -0,0 +1,16 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".inboxbaby_bat_codes", {
+  duration: 4000,
+  delay: 4000,
+});
+
+ScrollReveal().reveal(".gradient-text", {
+  duration: 2000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".busca", {
+  duration: 2000,
+  delay: 2000,
+});
